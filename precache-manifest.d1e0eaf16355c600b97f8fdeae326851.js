049f3fa84913d337c3e886721d92c70be0e94ddc
liamst19 <50284753+liamst19@users.noreply.github.com>
2020-01-28 01:04:06 +0900

    Updates

---
+++ precache-manifest.d1e0eaf16355c600b97f8fdeae326851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba3e6d98d3978df6cecddfa549c0dede",
+    "url": "/react-fetch-text/index.html"
+  },
+  {
+    "revision": "a9482e289ad222c1a148",
+    "url": "/react-fetch-text/static/css/main.eb8e008c.chunk.css"
+  },
+  {
+    "revision": "6fb9656793937ad77455",
+    "url": "/react-fetch-text/static/js/2.ad1511f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-fetch-text/static/js/2.ad1511f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9482e289ad222c1a148",
+    "url": "/react-fetch-text/static/js/main.7d5cce0f.chunk.js"
+  },
+  {
+    "revision": "57a2b5d533eb2ae4629f",
+    "url": "/react-fetch-text/static/js/runtime-main.1358e2ce.js"
+  }
+]);

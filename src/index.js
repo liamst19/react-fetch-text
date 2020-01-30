c90d1d39103122ae46030a5f7e421c69a62d1116
liamst19 <50284753+liamst19@users.noreply.github.com>
2020-01-30 20:06:40 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import JapaneseText from "./component.JapaneseText.js";
 import * as serviceWorker from "./serviceWorker";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Fetch Text Demo</h1>
+      </header>
+      <main>{<JapaneseText url="wagahai.json" />}</main>
+      <footer className="App-footer">
+        <h2>
+          <span>This is the footer.</span>
+        </h2>
+      </footer>
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
